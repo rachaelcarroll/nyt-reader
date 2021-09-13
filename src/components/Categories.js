0061fc6d35c17a779b94dfcf2c7355a522c0bfe8
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-13 18:09:27 -0400

    Track down bug in switching categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,16 @@
 
 export const Categories = ({getArticles}) => {
 
+    const setCategory = (category) => {
+        console.log('CATEGORY CLICKED?', category)
+        getArticles(category.category)
+    }
+
     const newsCategories = ['home', 'arts', 'science', 'us', 'world']
     const eachCategory = newsCategories.map((category, i) => {
+        console.log("?????", category)
         return (
-            <div className='category-tab' key={i} onClick={({category}) => getArticles({category})}>
+            <div className={`category-tab${i}`} key={i} onClick={() => setCategory({category})}>
                 {category}
             </div>
         )});
