6f3b6be8f0935007950b0a1af39d3fe1788121ad
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-13 16:05:14 -0400

    Build out ArticlePreview Component

---
+++ src/components/ArticlePreview.js
@@ -0,0 +1,24 @@
+import { ArticleContainer } from './ArticleContainer';
+import { NavLink } from 'react-router-dom';
+
+
+export default const ArticlePreview = ({id, title, media, description, link, author, datePublished}) => {
+
+    const imgAlt= `image of ${title}`
+    
+    return (
+        <article className='article-preview'>
+            <div className='article-img'>
+                <img src={media[1].url} alt={imgAlt}/>
+            </div>
+            <div className='article-description'>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+            <div className='date-author'>
+                <p>{datePublished}</p>
+                <p>{author}</p>
+            </div>
+        </article>
+    );
+}
